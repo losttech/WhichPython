1a13c355a32168a1ead92b758ed0f5abad3b9f23
Victor Milovanov <lost@losttech.software>
2019-06-13 12:51:22 -0700

    detect user-installed Python interpreters, not just system-wide
    also added some documentation
    
    addresses #3

--- src/PythonEnvironment.cs
+++ src/PythonEnvironment.cs
@@ -9,16 +9,43 @@
     using System.Text.RegularExpressions;
     using System.Threading;
     using Microsoft.Win32;
-
     using static System.FormattableString;
 
+    /// <summary>
+    /// Provides information about a Python environment
+    /// </summary>
     public class PythonEnvironment {
+        /// <summary>
+        /// Path to Python executable.
+        /// Usually <c>python.exe</c> on Windows and <c>pythonX.X</c> on Linux
+        /// </summary>
         public string InterpreterPath { get; }
+        /// <summary>
+        /// The value of interpreter's PYTHONHOME
+        /// </summary>
         public string Home { get; }
+        /// <summary>
+        /// Path to the Python dynamic library (useful for embedding). E.g. <c>python36.dll</c> on Windows.
+        /// </summary>
         public string DynamicLibraryPath {get;set;}
+        /// <summary>
+        /// Python language version.
+        /// </summary>
         public Version LanguageVersion { get; }
+        /// <summary>
+        /// Interpreter architecture.
+        /// </summary>
         public Architecture? Architecture { get; }
 
+        /// <summary>
+        /// Creates new instance of <see cref="PythonEnvironment"/>
+        /// </summary>
+        /// <param name="interpreterPath">Path to Python executable. Mandatory.
+        /// Usually <c>python.exe</c> on Windows and <c>pythonX.X</c> on Linux.</param>
+        /// <param name="home">The value of interpreter's PYTHONHOME. Optional.</param>
+        /// <param name="dll">Path to the Python dynamic library (useful for embedding). Optional.</param>
+        /// <param name="languageVersion">Python language version. Optional.</param>
+        /// <param name="architecture">Interpreter architecture</param>
         public PythonEnvironment(string interpreterPath, string home, string dll, Version languageVersion, Architecture? architecture) {
             this.InterpreterPath = interpreterPath ?? throw new ArgumentNullException(nameof(interpreterPath));
             this.Home = home;
@@ -27,17 +54,24 @@
             this.Architecture = architecture;
         }
 
+        /// <summary>
+        /// Enumerate Python interpreters, installed for current user (including system-wide ones).
+        /// <para>SECURITY: this invokes any files matching <c>python*</c> mask in PATH to determine if they are a Python interpreter.</para>
+        /// </summary>
         public static IEnumerable<PythonEnvironment> EnumerateEnvironments(CancellationToken cancellation = default) {
             var found = new SortedSet<string>();
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                foreach (var environment in EnumerateWindowsEnvironments(found, cancellation))
+                foreach (var environment in EnumerateRegistryEnvironments(found, cancellation))
                     yield return environment;
 
             foreach(var environment in EnumeratePathEnvironments(found, cancellation))
                 yield return environment;
         }
 
+        /// <summary>
+        /// Enumerate Conda environments of the current user.
+        /// </summary>
         public static IEnumerable<PythonEnvironment> EnumerateCondaEnvironments(CancellationToken cancellation = default) {
             string userHome = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
             if (userHome == null)
@@ -58,12 +92,19 @@
                     continue;
                 cancellation.ThrowIfCancellationRequested();
 
-                var environment = DetectEnvironment(home, cancellation);
+                var environment = DetectCondaEnvironment(home, cancellation);
                 if (environment != null)
                     yield return environment;
             }
         }
 
+        /// <summary>
+        /// Enumerate Python interpreters in the specified directories.
+        /// Does not go into subdirectories.
+        /// <para>SECURITY: this invokes any files matching the mask to determine if they are a Python interpreter.</para>
+        /// </summary>
+        /// <param name="paths">Directories to search for interpreters.</param>
+        /// <param name="interpreterFileNameMask">What an interpreter file name is expected to look like.</param>
         public static IEnumerable<PythonEnvironment> EnumeratePathEnvironments(IEnumerable<string> paths, string interpreterFileNameMask = "python*", CancellationToken cancellation = default){
             if (paths == null) throw new ArgumentNullException(nameof(paths));
             if (interpreterFileNameMask == null) throw new ArgumentNullException(nameof(interpreterFileNameMask));
@@ -81,6 +122,9 @@
             }
         }
 
+        /// <summary>
+        /// Enumerate Python environments, listed in the PATH environment variable.
+        /// </summary>
         static IEnumerable<PythonEnvironment> EnumeratePathEnvironments(ISet<string> enumerated, CancellationToken cancellation){
             bool windows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
             char pathSeparator = windows ? ';' : ':';
@@ -94,16 +138,23 @@
                     yield return env;
         }
 
-        static IEnumerable<PythonEnvironment> EnumerateWindowsEnvironments(ISet<string> enumerated, CancellationToken cancellation) {
-            using (var systemEnvironments = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Python\PythonCore"))
-                if (systemEnvironments != null) {
+        static IEnumerable<PythonEnvironment> EnumerateRegistryEnvironments(ISet<string> enumerated, CancellationToken cancellation) {
+            foreach (var environment in EnumerateRegistryEnvironments(Registry.CurrentUser, enumerated, cancellation))
+                yield return environment;
+            foreach (var environment in EnumerateRegistryEnvironments(Registry.LocalMachine, enumerated, cancellation))
+                yield return environment;
+        }
+
+        static IEnumerable<PythonEnvironment> EnumerateRegistryEnvironments(RegistryKey hive, ISet<string> enumerated, CancellationToken cancellation) {
+            using (var registryEnvironments = hive.OpenSubKey(@"SOFTWARE\Python\PythonCore"))
+                if (registryEnvironments != null) {
                     var abi = RuntimeInformation.ProcessArchitecture;
-                    foreach (var environment in EnumerateEnvironments(enumerated, systemEnvironments, abi, cancellation))
+                    foreach (var environment in EnumerateRegistryEnvironments(enumerated, registryEnvironments, abi, cancellation))
                         yield return environment;
                 }
         }
 
-        static IEnumerable<PythonEnvironment> EnumerateEnvironments(ISet<string> enumerated, RegistryKey registryKey, Architecture abi, CancellationToken cancellation) {
+        static IEnumerable<PythonEnvironment> EnumerateRegistryEnvironments(ISet<string> enumerated, RegistryKey registryKey, Architecture abi, CancellationToken cancellation) {
             foreach (string majorVersion in registryKey.GetSubKeyNames()) {
                 cancellation.ThrowIfCancellationRequested();
 
@@ -142,7 +193,7 @@
           : RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? ".dylib"
           : ".so";
 
-        static PythonEnvironment DetectEnvironment(string home, CancellationToken cancellation) {
+        static PythonEnvironment DetectCondaEnvironment(string home, CancellationToken cancellation) {
             if (!Directory.Exists(home))
                 return null;
 
