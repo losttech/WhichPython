0fac17f307b47ac9ded2416e1a585b4eec4c9078
Victor Nova <lost@losttech.software>
2021-04-18 18:34:52 -0700

    added app option to list only home directories of discovered python environments

--- app/ListCommand.cs
+++ app/ListCommand.cs
@@ -8,14 +8,21 @@
         public override int Run(string[] remainingArguments) {
             foreach (var environment in PythonEnvironment.EnumerateEnvironments()
                                     .Concat(CondaEnvironment.EnumerateCondaEnvironments())) {
-                Console.WriteLine($"{environment.LanguageVersion?.ToString(2) ?? "??"}-{environment.Architecture?.ToString() ?? "???"} @ {environment.Home}");
+                Console.WriteLine(this.HomeOnly
+                    ? environment.Home.FullName
+                    : $"{environment.LanguageVersion?.ToString(2) ?? "??"}-{environment.Architecture?.ToString() ?? "???"} @ {environment.Home}");
             }
 
             return 0;
         }
 
+        public bool HomeOnly { get; set; }
+
         public ListCommand() {
             this.IsCommand("list");
+
+            this.HasOption("--home-only", "Only print home directory for each environment",
+                onOff => this.HomeOnly = onOff == "on");
         }
     }
 }
